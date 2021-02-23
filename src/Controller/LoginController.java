7a68436c64920a61f07d4d779d09ac1b2437f4b5
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-23 00:45:32 -0600

    created login view

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,41 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label passwordLabel1;
+
+    @FXML
+    private TextField passwordTextField1;
+
+    @FXML
+    private TextArea welcomeMessageLabel;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../View/login.fxml"));
+        primaryStage.setTitle("Scheduly");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
