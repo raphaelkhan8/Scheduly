efc89b4372ec2b7807f1b518c4ec706788bd453a
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-23 00:53:57 -0600

    connected to database

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -1,2 +1,43 @@
-package Database;public class DBConnection {
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String protocol = DBInfo.getProtocol();
+    private static final String vendorName = DBInfo.getVendorName();
+    private static final String ipAddress = DBInfo.getIpAddress();
+    private static final String dbName = DBInfo.getDbName();
+
+    private static final String jdbcUrl = protocol + vendorName + ipAddress + dbName;
+    private static final String MySQLJBCDriver = "com.mysql.cj.jdbc.Driver";
+
+    private static final String username = DBInfo.getUsername();
+    private static final String password = DBInfo.getPassword();
+    private static Connection conn = null;
+
+    public static Connection startConnection() {
+        try {
+            Class.forName(MySQLJBCDriver);
+            conn = DriverManager.getConnection(jdbcUrl, username, password);
+
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void stopConnection() {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
