2365d14eaa76f5a71d1699f3a358c056ee018d05
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-24 16:43:45 -0600

    removed unnecessary imports

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,18 +1,11 @@
 package Main;
 
 import Database.DBConnection;
-import Database.DBQuery;
-import Model.Country;
-import com.mysql.cj.jdbc.result.ResultSetImpl;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.sql.SQLException;
 
 public class Main extends Application {
 
