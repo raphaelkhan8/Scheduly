d1e4b76a8a5130f4441caf73d5c101167f5136b0
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-24 17:24:01 -0600

    added controller boilerplate code

--- src/Controller/HomePageController.java
+++ src/Controller/HomePageController.java
@@ -1,2 +1,84 @@
-package Controller;public class HomePageController {
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HomePageController {
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Label optionsLabel;
+
+    @FXML
+    private Label customerLabel;
+
+    @FXML
+    private Label appointmentLabel;
+
+    @FXML
+    private Label reportLabel;
+
+    @FXML
+    private Button customerTableButton;
+
+    @FXML
+    private Button appointmentTableButton;
+
+    @FXML
+    private Label customerLabel1;
+
+    @FXML
+    private Label appointmentLabel1;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button reportTableButton;
+
+    @FXML
+    private Label reportLabel1;
+
+    @FXML
+    private Label reportLabel2;
+
+    @FXML
+    private Label reportLabel3;
+
+    @FXML
+    private Label reportLabel4;
+
+    @FXML
+    void cancelView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openAptTableView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCustomerTableView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openReportTableView(ActionEvent event) {
+
+    }
+
 }
+
