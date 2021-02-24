8a39c07aae041656d222dc949ec15f6428b113e5
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-23 20:55:31 -0600

    tested database query code...worked

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,11 +2,17 @@ package Main;
 
 import Database.DBConnection;
 import Database.DBQuery;
+import Model.Country;
+import com.mysql.cj.jdbc.result.ResultSetImpl;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -19,8 +25,17 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        DBConnection.startConnection();
+    public static void main(String[] args) throws SQLException {
+        ObservableList<Country> countries = FXCollections.observableArrayList();
+        DBQuery.makeQuery("SELECT * FROM countries");
+        ResultSetImpl rs = (ResultSetImpl) DBQuery.getResult();
+        while (rs.next()) {
+            int countryId = rs.getInt("Country_ID");
+            String country = rs.getString("Country");
+            Country newCountry = new Country(countryId, country);
+            countries.add(newCountry);
+            System.out.println("Country ID: " + newCountry.getCountryId() + "\nCountry Name: " + newCountry.getCountryName() + "\n");
+        }
         launch(args);
         DBConnection.stopConnection();
     }
