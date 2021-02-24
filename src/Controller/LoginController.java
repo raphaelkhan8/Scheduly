5e3dee8617fe08e622b6e2345e3769ee9c9d48ec
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-23 21:29:08 -0600

    input login info successfully compares against db

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,11 +1,11 @@
 package Controller;
 
+import Database.DBQuery;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.sql.ResultSet;
 
 public class LoginController {
 
@@ -35,7 +35,36 @@ public class LoginController {
 
     @FXML
     void handleLogin(ActionEvent event) {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
 
+        boolean proceed = checkLoginInfo(username, password);
+
+        if (proceed) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "User successfully logged in.");
+            alert.showAndWait();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "The username and password did not match.");
+            alert.showAndWait();
+        }
+    }
+
+    public static Boolean checkLoginInfo(String username, String password) {
+        try {
+            DBQuery.makeQuery("SELECT * FROM users");
+            ResultSet rs = DBQuery.getResult();
+
+            while(rs.next()) {
+                if(rs.getString("User_Name").equals(username) && rs.getString("Password").equals(password))
+                    return true;
+                }
+                return false;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
 }
