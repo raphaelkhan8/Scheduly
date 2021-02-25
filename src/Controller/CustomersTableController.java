b4e8af4e6a27ae7a2e2eb45bbd652d4f4910e372
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-24 21:00:56 -0600

    added CustomersTable view

---
+++ src/Controller/CustomersTableController.java
@@ -0,0 +1,114 @@
+package Controller;
+
+import Model.SessionHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomersTableController implements Initializable {
+
+    @FXML
+    private Label customersTableHeader;
+
+    @FXML
+    private TableView<?> customersTableView;
+
+    @FXML
+    private TableColumn<?, ?> customerIDTable;
+
+    @FXML
+    private TableColumn<?, ?> customerNameTable;
+
+    @FXML
+    private TableColumn<?, ?> customerAddressTable;
+
+    @FXML
+    private TableColumn<?, ?> customerCityTable;
+
+    @FXML
+    private TableColumn<?, ?> customerCountryTable;
+
+    @FXML
+    private TableColumn<?, ?> customerPostalCodeTable;
+
+    @FXML
+    private TableColumn<?, ?> customerPhoneTable;
+
+    @FXML
+    private Button customersTableAddButton;
+
+    @FXML
+    private Button customersTableUpdateButton;
+
+    @FXML
+    private Button customersTableDeleteButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Label customersTableButtonMessage;
+
+    @FXML
+    private Label addAppointmentButtonMessage;
+
+    @FXML
+    private Button appointmentsTableAddButton;
+
+    // var to hold user's language
+    ResourceBundle userLanguage = SessionHandler.getUserLanguage();
+
+    // change text to match user's language upon initialization
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        customersTableHeader.setText(userLanguage.getString("customersTableHeader"));
+        customersTableButtonMessage.setText(userLanguage.getString("customersTableButtonMessage"));
+        addAppointmentButtonMessage.setText(userLanguage.getString("addAppointmentButtonMessage"));
+        appointmentsTableAddButton.setText(userLanguage.getString("appointmentsTableAddButton"));
+        customersTableAddButton.setText(userLanguage.getString("customersTableAddButton"));
+        customersTableUpdateButton.setText(userLanguage.getString("customersTableUpdateButton"));
+        customersTableDeleteButton.setText(userLanguage.getString("customersTableDeleteButton"));
+        cancelButton.setText(userLanguage.getString("cancelButton"));
+        customerIDTable.setText(userLanguage.getString("ID"));
+        customerNameTable.setText(userLanguage.getString("Name"));
+        customerAddressTable.setText(userLanguage.getString("Address"));
+        customerCityTable.setText(userLanguage.getString("City"));
+        customerCountryTable.setText(userLanguage.getString("Country"));
+        customerPostalCodeTable.setText(userLanguage.getString("Postal_Code"));
+        customerPhoneTable.setText(userLanguage.getString("Phone"));
+    }
+
+    @FXML
+    void addAppointmentTableHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addCustomerHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteCustomerHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateCustomerHandler(ActionEvent event) {
+
+    }
+
+}
