d2440d4b901bacf6d273ad2cefe9659e947b6603
Raphael Khan <raphaelkhan8@gmail.com>
2021-02-25 20:21:24 -0600

    added French text to Add Customer view

--- src/Controller/AddCustomerController.java
+++ src/Controller/AddCustomerController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -19,22 +20,46 @@ import java.util.ResourceBundle;
 public class AddCustomerController implements Initializable {
 
     @FXML
-    private TextField addCustomerText;
+    private Label addCustomerHeader;
+
+    @FXML
+    private Label addCustomerHeaderText;
+
+    @FXML
+    private Label addCustomerNameLabel;
+
+    @FXML
+    private Label addCustomerAddressLabel;
+
+    @FXML
+    private Label addCustomerCountryLabel;
+
+    @FXML
+    private Label addCustomerDivisionLabel;
+
+    @FXML
+    private Label addCustomerPostalLabel;
+
+    @FXML
+    private Label addCustomerPhoneLabel;
+
+    @FXML
+    private TextField addCustomerNameText;
 
     @FXML
     private TextField addCustomerAddressText;
 
     @FXML
-    private TextField addCustomerPostalCodeText;
+    private TextField addCustomerPostalText;
 
     @FXML
-    private TextField addCustomPhoneText;
-
-    @FXML
-    private ComboBox<?> addCustomerCityComboBox;
+    private TextField addCustomerPhoneText;
 
     @FXML
     private ComboBox<?> addCustomerCountryComboBox;
+
+    @FXML
+    private ComboBox<?> addCustomerDivisionComboBox;
 
     @FXML
     private Button saveCustomerButton;
@@ -49,15 +74,26 @@ public class AddCustomerController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         cancelButton.setText(userLanguage.getString("cancelButton"));
-    }
-
-    @FXML
-    void addCustomerCityHandler(ActionEvent event) {
-
+        saveCustomerButton.setText(userLanguage.getString("saveButton"));
+        addCustomerHeader.setText(userLanguage.getString("addCustomerHeader"));
+        addCustomerHeaderText.setText(userLanguage.getString("addCustomerHeaderText"));
+        addCustomerNameLabel.setText(userLanguage.getString("Name"));
+        addCustomerAddressLabel.setText(userLanguage.getString("Address"));
+        addCustomerCountryLabel.setText(userLanguage.getString("Country"));
+        addCustomerDivisionLabel.setText(userLanguage.getString("Division"));
+        addCustomerPostalLabel.setText(userLanguage.getString("Postal_Code"));
+        addCustomerPhoneLabel.setText(userLanguage.getString("Phone"));
+        addCustomerCountryComboBox.promptTextProperty().setValue(userLanguage.getString("selectCountry"));
+        addCustomerDivisionComboBox.promptTextProperty().setValue(userLanguage.getString("selectDivision"));
     }
 
     @FXML
     void addCustomerCountryHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addCustomerDivisionHandler(ActionEvent event) {
 
     }
 
